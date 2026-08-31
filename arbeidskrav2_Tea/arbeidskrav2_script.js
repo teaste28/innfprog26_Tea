54cdc0ad71d798e87f30ecc586b620bbb1d9840f
TeawAcuteAccent <teasteen@icloud.com>
2026-08-31 11:17:03 +0200

    cheat_sheet variabler og arbeidskrav mapper
    Startet på cheat_sheet for variabler og lagt til mapper for arbeidskrav 1 og 2

---
+++ arbeidskrav2_Tea/arbeidskrav2_script.js
@@ -0,0 +1,31 @@
+const students = [
+    { name: "Alice", age: 20, grade: "6", workexperience: 2 },
+    { name: "Bob", age: 22, grade: "5", workexperience: 1 },
+    { name: "Charlie", age: 19, grade: "4", workexperience: 0 },
+    { name: "David", age: 21, grade: "5", workexperience: 3 },
+    { name: "Eve", age: 23, grade: "6", workexperience: 4 },
+    { name: "Frank", age: 20, grade: "3", workexperience: 1 },
+    { name: "Grace", age: 22, grade: "2", workexperience: 2 },
+    { name: "Hannah", age: 39, grade: "1", workexperience: 5 },
+    { name: "Ian", age: 21, grade: "4", workexperience: 1 },
+    { name: "Jack", age: 23, grade: "5", workexperience: 3 },
+    { name: "Kathy", age: 20, grade: "6", workexperience: 4 },
+    { name: "Liam", age: 22, grade: "3", workexperience: 2 },
+    { name: "Mia", age: 19, grade: "2", workexperience: 1 },
+    { name: "Noah", age: 21, grade: "1", workexperience: 0 },
+    { name: "Olivia", age: 23, grade: "4", workexperience: 3 },
+    { name: "Paul", age: 40, grade: "5", workexperience: 10 },
+    { name: "Quinn", age: 22, grade: "6", workexperience: 0 },
+    { name: "Ryan", age: 19, grade: "3", workexperience: 0 },
+    { name: "Sophia", age: 21, grade: "2", workexperience: 0 },
+    { name: "Tyler", age: 23, grade: "1", workexperience: 0 }
+];
+
+const grades = [
+    { letter: "A", score: 6 },
+    { letter: "B", score: 5 },
+    { letter: "C", score: 4 },
+    { letter: "D", score: 3 },
+    { letter: "E", score: 2 },
+    { letter: "F", score: 1}
+]
